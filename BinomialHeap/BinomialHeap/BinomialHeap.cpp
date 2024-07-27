058409ec1ac9d49cd9f18eba064b1a317719bcd7
Makar Kireenko <137899428+kireemak@users.noreply.github.com>
2024-07-27 15:31:53 +0300

    Add files via upload

---
+++ BinomialHeap/BinomialHeap/BinomialHeap.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+
+std::vector <long long> getBinomialTreesHeights(unsigned long long n)
+{
+    std::vector <long long> heightsVector;
+
+    unsigned long long remainingBits = n;
+    unsigned long long currentTreeHeight = 0;
+    unsigned long long checkSum = 0;
+
+    while (remainingBits > 0)
+    {
+        if ((remainingBits & 1) == 1)
+        {
+            heightsVector.push_back(currentTreeHeight);
+            checkSum += (1 << currentTreeHeight);
+        }
+
+        remainingBits >>= 1;
+        currentTreeHeight++;
+    }
+
+    if (checkSum != n)
+    {
+        heightsVector.clear();
+        heightsVector.push_back(-1);
+    }
+
+    return heightsVector;
+}
+
+int main()
+{
+    std::ifstream fin("input.txt");
+    std::ofstream fout("output.txt");
+
+    unsigned long long n;
+    //std::cin >> n;
+    fin >> n;
+    std::vector <long long> answer = getBinomialTreesHeights(n);
+
+    for (unsigned long long i = 0; i < answer.size(); i++)
+    {
+        fout << answer[i] << std::endl;
+        std::cout << answer[i] << std::endl;
+    }
+
+    fin.close();
+    fout.close();
+
+    return 0;
+}
---
+++ Roads_Building/Roads_Building/Roads_Building.cpp
@@ -0,0 +1,100 @@
+﻿#include <iostream>
+#include <fstream>
+//#include <vector>
+
+class DSU
+{
+    //std::vector <int> parentVector, rankVector;
+    int* parentArray;
+    int* rankArray;
+
+public:
+
+    int componentsCount;
+
+    DSU(int elementsCount)
+    {
+        componentsCount = elementsCount;
+        parentArray = new int[elementsCount];
+        rankArray = new int[elementsCount];
+
+        for (int i = 0; i < elementsCount; i++)
+        {
+            rankArray[i] = 1;
+            parentArray[i] = i;
+        }
+    }
+    /*
+    int findRoot(int element)
+    {
+        if (element == parentArray[element])
+        {
+            return element;
+        }
+        return parentArray[element] = findRoot(parentArray[element]);
+    }
+    */
+
+    int findRoot(int node)
+    {
+        while (node != parentArray[node]) {
+            parentArray[node] = parentArray[parentArray[node]];
+            node = parentArray[node];
+        }
+        return node;
+    }
+
+
+    void unite(int elementA, int elementB)
+    {
+        int rootA = findRoot(elementA);
+        int rootB = findRoot(elementB);
+        if (rootA != rootB)
+        {
+            componentsCount--;
+            if (rankArray[rootA] < rankArray[rootB])
+            {
+                int buffer = rootA;
+                rootA = rootB;
+                rootB = buffer;
+            }
+            parentArray[rootB] = rootA;
+            if (rankArray[rootA] == rankArray[rootB])
+            {
+                rankArray[rootA]++;
+            }
+        }
+    }
+};
+
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+    std::ifstream fin("input.txt");
+    std::ofstream fout("output.txt");
+
+    int townsCount, roadsCount;
+    std::cin >> townsCount >> roadsCount;
+    //fin >> townsCount >> roadsCount;
+    
+    DSU townsDsu (townsCount);
+
+    for (int i = 0; i < roadsCount; i++)
+    {
+        int town1, town2;
+        std::cin >> town1 >> town2;
+        //fin >> town1 >> town2;
+
+        town1--;
+        town2--;
+
+        townsDsu.unite(town1, town2);
+        std::cout << townsDsu.componentsCount << std::endl;
+        //fout << townsDsu.componentsCount << std::endl;
+    }
+
+    fout.close();
+    fin.close();
+
+    return 0;
+}
