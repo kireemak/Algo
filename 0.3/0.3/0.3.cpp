af21de972f3af6c100e7232d0edb9f88fb258ef4
Makar Kireenko <137899428+kireemak@users.noreply.github.com>
2024-07-27 15:23:15 +0300

    Add files via upload

---
+++ 0.3/0.3/0.3.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+
+unsigned long long exponentiationByFerm(unsigned long long base, int exponent, int mod)
+{
+    if (exponent == 0)
+    {
+        return 1;
+    }
+
+    if (exponent % 2 == 1)
+    {
+        return ((exponentiationByFerm(base, exponent - 1, mod) % mod) * (base % mod)) % mod;
+    }
+    else
+    {
+        unsigned long long f = exponentiationByFerm(base, exponent / 2, mod) % mod;
+        return (f * f) % mod;
+    }
+}
+
+
+int main()
+{
+	int n, k;
+	int mod = 1000000007;
+
+    std::cin >> n >> k;
+
+    int* fact = new int[n + 1];
+    for (int i = 0; i <= n; i++)
+    {
+        if (i == 0)
+        {
+            fact[i] = 1;
+            continue;
+        }
+        fact[i] = (fact[i - 1] * i) % mod;
+    }
+
+	std::cout <<(fact[n] * exponentiationByFerm(fact[k], mod - 2, mod) % mod
+        * exponentiationByFerm(fact[n-k], mod - 2, mod) % mod) % mod << std::endl;
+
+    return 0;
+}
---
+++ lcs/lcs/lcs.cpp
@@ -0,0 +1,115 @@
+﻿#include <iostream>
+#include <vector>
+#include <fstream>
+
+int main()
+{
+	//std::ofstream fout("output.txt");
+	//std::ifstream fin("input.txt");
+
+	int n;
+    std::cin >> n;
+	//fin >> n;
+
+	std::vector <int> indexes1;
+	std::vector <int> indexes2;
+	int* arr1 = new int[n];
+	int* arr2 = new int[n];
+	for (int i = 0; i < n; i++)
+	{
+		std::cin >> arr1[i];
+		//fin >> arr1[i];
+	}
+	for (int i = 0; i < n; i++)
+	{
+		std::cin >> arr2[i];
+		//fin >> arr2[i];
+	}
+
+	int** matrix = new int*[n + 1];
+	for (int i = 0; i < n + 1; i++)
+	{
+		matrix[i] = new int[n + 1];
+	}
+
+	for (int i = 0; i < n + 1; i++)
+	{
+		for (int j = 0; j < n + 1; j++)
+		{
+			if (i == 0 || j == 0)
+			{
+				matrix[i][j] = 0;
+			}
+		}
+	}
+
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= n; j++)
+		{
+			if (arr1[i - 1] == arr2[j - 1])
+			{
+				matrix[i][j] = matrix[i - 1][j - 1] + 1;
+			}
+			else
+			{
+				if (matrix[i - 1][j] > matrix[i][j - 1])
+				{
+					matrix[i][j] = matrix[i - 1][j];
+				}
+				else
+				{
+					matrix[i][j] = matrix[i][j - 1];
+				}
+			}
+		}
+	}
+
+	
+	for (int k = n, l = n; matrix[k][l] != 0; k--, l--)
+	{
+		if (arr1[k - 1] == arr2[l - 1])
+		{
+			indexes1.push_back(k - 1);
+			indexes2.push_back(l - 1);
+			continue;
+		}
+		else
+		{
+			if (matrix[k][l] == matrix[k - 1][l])
+			{
+				l++;
+			}
+			else if (matrix[k][l] == matrix[k][l - 1])
+			{
+				k++;
+			}
+		}
+	}
+
+	std::cout << matrix[n][n] << std::endl;
+
+	for (int i = matrix[n][n] - 1; i >= 0; i--)
+	{
+		std::cout << indexes1.at(i) << " ";
+	}
+	std::cout << std::endl;
+
+	for (int i = matrix[n][n] - 1; i >= 0; i--)
+	{
+		std::cout << indexes2.at(i) << " ";
+	}
+	
+	delete[] arr1;
+	delete[] arr2;
+
+	for (int i = 0; i <= n; i++)
+	{
+		delete[] matrix[i];
+	}
+
+	delete[] matrix;
+
+	//fin.close();
+	return 0;
+}
